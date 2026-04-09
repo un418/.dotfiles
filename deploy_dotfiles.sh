e9ba436671888a152124f9da5515a01b5472e6a1
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-09 17:54:04 +0100

    add common aliases

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -5,6 +5,7 @@ ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 script_dir="${0:A:h}"
 
 ln -sf "$script_dir"/42.aliases "$ZSH_CUSTOM"/42_aliases.zsh
+ln -sf "$script_dir"/common.aliases "$ZSH_CUSTOM"/common_aliases.zsh
 ln -sf "$script_dir"/common.functions "$ZSH_CUSTOM"/common_functions.zsh
 
 echo "Sucesss: Dotfiles deployed"
