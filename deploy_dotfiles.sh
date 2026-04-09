5e1615857c89dcf0f3a3e19dcc371032991848c8
Adam Fernandes <adaferna@student.42lisboa.com>
2026-04-09 17:41:47 +0100

    improve install script

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 set -e -u -o pipefail
 
-ln -s 42.aliases $ZSH_CUSTOM/42_aliases.zsh
-ln -s common.functions $ZSH_CUSTOM/common_functions.zsh
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+script_dir="${0:A:h}"
+
+ln -sf "$script_dir"/42.aliases "$ZSH_CUSTOM"/42_aliases.zsh
+ln -sf "$script_dir"/common.functions "$ZSH_CUSTOM"/common_functions.zsh
+
+echo "Sucesss: Dotfiles deployed"
+echo "- - -"
+echo "Remember to source source your .zshrc on your current file now"
+echo "Run \`source ~/.zshrc\` or start new shell \`zsh\`"
